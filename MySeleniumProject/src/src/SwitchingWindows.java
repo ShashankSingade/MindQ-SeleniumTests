17c025e4bf275fa8650439bec988a4d08491210f
shash <shash@192.168.86.210>
2019-08-11 17:49:49 +0530

    Changes

--- MySeleniumProject/src/src/SwitchingWindows.java
+++ MySeleniumProject/src/src/SwitchingWindows.java
@@ -10,31 +10,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SwitchingWindows {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		String chromepath = "C:\\MySelenium_workspace\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", chromepath);
 		String baseURL = "https://learn.letskodeit.com/p/practice";
-		
-		WebDriver driver= new ChromeDriver();
+
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.get(baseURL);
-		
+
 		String parentWindow = driver.getWindowHandle();
 		System.out.println("The parent window handle is: " + parentWindow);
-		
+
 		WebElement openWindow = driver.findElement(By.id("openwindow"));
 		openWindow.click();
-		
+
 		Set<String> allWindowHandles = driver.getWindowHandles();
-		
-		
-		for(String handle: allWindowHandles)
+
+		for (String handle : allWindowHandles)
+
 		{
 			System.out.println("The current window handle is: " + handle);
-			
-			if(!handle.equals(parentWindow))
-			{
+
+			if (!handle.equals(parentWindow)) {
 				driver.switchTo().window(handle);
 				driver.manage().window().maximize();
 				Thread.sleep(3000);
@@ -45,16 +44,14 @@ public class SwitchingWindows {
 				driver.close();
 				break;
 			}
-			
-			
+
 		}
-		
-		//Switching back to parent window
+
+		// Switching back to parent window
 		driver.switchTo().window(parentWindow);
 		driver.findElement(By.id("name")).sendKeys("Switching tested successfully");
 		Thread.sleep(3000);
 		driver.quit();
-				
 
 	}
 
